c84429840d9c63db738a874f627051a6d8172d8b
Seu Nome <seu@email.com>
2025-05-30 12:28:32 -0300

    fix: adiciona i.postimg.cc aos domínios permitidos para imagens

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   // Configurações opcionais do Next.js
   reactStrictMode: true,
   images: {
-    domains: ['localhost', 'vercel.app'],
+    domains: ['localhost', 'vercel.app', 'i.postimg.cc'],
   },
   // Habilita o output estático para facilitar o deploy
   eslint: {
